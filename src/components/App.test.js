439c1a4a5d2c60f83f43f618f9fff61d554c7492
Sandesh Kamble <sandesh.uk001@gmail.com>
2021-11-04 13:23:11 +0530

    travis config update for building and pushing image

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,1 +1,12 @@
-it("renders without crashing", () => {});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain('App2');
+
+  ReactDOM.unmountComponentAtNode(div);
+});
